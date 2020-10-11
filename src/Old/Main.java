bed7c1adf0df62867835729c4835137fd1b2a13e
AdrianRuizB18 <72421981+AdrianRuizB18@users.noreply.github.com>
2020-10-10 22:28:49 -0500

    Update Main.java

--- src/Old/Main.java
+++ src/Old/Main.java
@@ -4,5 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
 	      System.out.println("Hello my nmae is Cristhians Ruiz.");
+	      System.out.println("Hello Cristhians, My name is Adrian.");
     }
 }
